db40a6ca464c1338fcefe7063766dee51eac1ee9
Jeo98 <evaristoolivares.jeo@gmail.com>
2024-09-16 16:00:30 -0300

    primeros pasos

---
+++ decisiones.java
@@ -0,0 +1,12 @@
+public class decisiones{
+
+    public static void main(String[] args) {
+
+        int fechaLanzamiento = 1999;
+        if (fechaLanzamiento > 2022){
+
+            System.out.println("Peliculas más actuales");
+
+        }
+    }
+}
---
+++ main.java
@@ -0,0 +1,14 @@
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a JEOSCREEN");
+        System.out.println("Pelicula: Armagedon");
+        int fechaLanzamiento= 1999;
+        double calificacion= 7.1;
+        double media= (8.1+6.0+2)/3;
+        System.out.println(media);
+        String informacion = "Pelicula lanzada en: \n" + fechaLanzamiento + "\n disponible: si\n" +  "Calificacion: "+ calificacion;
+
+        System.out.println(informacion);
+    }
+}
